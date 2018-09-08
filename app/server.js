4e1d70241d6d4fb85eaedfcd0a6926fcd69cc887
CGrant303 <chrisgrant303@gmail.com>
2018-09-08 15:44:19 -0500

    completed server logic

--- app/server.js
+++ app/server.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+
+const app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+require("./routing/apiRoutes.js");
+require("./routing/htmlRoutes.js");
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});

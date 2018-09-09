ee1f238c000cd05b2d2eb4ff89dcd754a743968a
Chris Grant <chrisgrant303@gmail.com>
2018-09-09 10:43:02 -0500

    POST logic confirmed, moving onto match logic

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friendData = [
+    {
+        "name": "Chris Grant",
+        "photo": "https://randomuser.me/api/portraits/men/83.jpg",
+        "scores": [5, 6, 7, 3, 5, 8, 2, 3, 9, 1]
+    },
+    {
+        "name": "Jeff Miller",
+        "photo": "https://randomuser.me/api/portraits/men/12.jpg",
+        "scores": [2, 3, 7, 3, 5, 6, 2, 9, 2, 9]
+    },
+    {
+        "name": "Andre Aggasi",
+        "photo": "https://randomuser.me/api/portraits/men/16.jpg",
+        "scores": [1, 3, 4, 5, 7, 8, 4, 2, 4, 2]
+    },
+    {
+        "name": "Wilson Heavy",
+        "photo": "https://randomuser.me/api/portraits/men/46.jpg",
+        "scores": [6, 6, 6, 6, 6, 3, 4, 2, 5, 7]
+    },
+    {
+        "name": "Sean Deaner",
+        "photo": "https://randomuser.me/api/portraits/men/10.jpg",
+        "scores": [7, 4, 1, 9, 8, 3, 4, 6, 2, 7]
+    },
+    {
+        "name": "Leslie Wowzer",
+        "photo": "https://randomuser.me/api/portraits/women/90.jpg",
+        "scores": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+    },
+    {
+        "name": "Stephanie Smith",
+        "photo": "https://randomuser.me/api/portraits/women/91.jpg",
+        "scores": [2, 4, 6, 8, 1, 3, 5, 7, 2, 7]
+    },
+    {
+        "name": "Beth Shaker",
+        "photo": "https://randomuser.me/api/portraits/women/92.jpg",
+        "scores": [7, 4, 1, 9, 2, 3, 4, 6, 6, 1]
+    },
+    {
+        "name": "Amy Murphy",
+        "photo": "https://randomuser.me/api/portraits/women/93.jpg",
+        "scores": [4, 8, 3, 5, 5, 4, 4, 3, 5, 2]
+    },
+    {
+        "name": "Maxine Magnificent",
+        "photo": "https://randomuser.me/api/portraits/women/94.jpg",
+        "scores": [2, 1, 5, 5, 2, 1, 4, 7, 2, 7]
+    }
+];
+
+module.exports = friendData; 
--- app/server.js
+++ app/server.js
@@ -1,19 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 
-const app = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-const PORT = 8080;
+var PORT = 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./routing/apiRoutes.js");
-require("./routing/htmlRoutes.js");
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
